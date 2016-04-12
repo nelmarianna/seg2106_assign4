1af81bef857416aaf969f59464502fa007c2c6dd
Lucia <lucia.okeh@gmail.com>
2016-04-12 00:31:15 -0400

    added stuff to main

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
-
+import java.util.Random;
 public class Main {
 
+  public static void main(String args[]) {
+  
+  Patient p1 = new Patient("NEA");
+  Patient p2 = new Patient("NEA");
+  Patient p3 = new Patient("CW");
+
+  }
 }

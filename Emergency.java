557415dae4888334018340707564a7b800adb6bc
nelmarianna <aniela.opolski@gmail.com>
2016-04-12 00:31:06 -0400

    nurse implemented

--- Emergency.java
+++ Emergency.java
@@ -1,31 +1,22 @@
 
 public class Emergency{
 
-
+  Globals vars;
   Queue q = new Queue();
+  Nurse nOne, nTwo, nThree;
   
   public Emergency(Patient p){
     q.enqueue(p);
   }
- //;pllol
- //has all the queues here???
+  public void Setting(){
+	  vars.set(q);
+  }
+  
+  public void Categorize(){
+	  nOne = new Nurse();
+	  nTwo = new Nurse();
+	  nThree = new Nurse();
+  }
  
- //has all the queues here???
- public static void main(String [] args){
-  
-  //person comes into emergency room
-
-  
-  //three new threads one per nurse???
-  
-  //nurse classifies CW or NEA
-    //added to queue
-    //doctor group a treats NEA queue
-    //doctor group b treats CW queue
-    //if NEA needs tests-> gets added to lab queue
- }
-
-
-
 
 }
--- Nurse.java
+++ Nurse.java
@@ -1,15 +1,39 @@
+import java.util.Random;
 
 public class Nurse implements Runnable{
-	String group;
-
-	Nurse(String group)
+	Globals vars;
+	Patient p;
+	Random r = new Random();
+	
+	Nurse()
 	{
-		this.group=group;
+		(new Thread(new Nurse())).start();
+		
 	}
 
-	@Override
+	
 	public void run() {
-		// TODO Auto-generated method stub
+		try {
+			
+			while(!vars.get().isEmpty()){
+			
+			Thread.sleep(250);
+			  p=(Patient)vars.get().dequeue();
+			
+			  if (r.nextInt(100) < 15)
+			  {
+			    vars.getNEA().enqueue(p);
+			  }
+			  else
+			  {
+				  vars.getCW().enqueue(p);
+			  }
+			  this.notifyAll();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 }

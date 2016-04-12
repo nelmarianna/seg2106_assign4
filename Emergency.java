b10f493c6bd3d19d45bfe9fc11c0ef253aa26964
Lucia <lucia.okeh@gmail.com>
2016-04-11 23:25:38 -0400

    added neels nurses

--- Emergency.java
+++ Emergency.java
@@ -1,5 +1,5 @@
 
-public class Emergency implements Runnable{
+public class Emergency{
 
 
   Queue q = new Queue();
@@ -10,29 +10,5 @@ public class Emergency implements Runnable{
  //;pllol
  //has all the queues here???
 
- 
- //has all the queues here???
- public static void main(String [] args){
-  
-  //person comes into emergency room
-
-  
-  //three new threads one per nurse???
-  (new Thread(new Emergency())).start();
-  (new Thread(new Emergency())).start();
-  (new Thread(new Emergency())).start();
-  
-  //nurse classifies CW or NEA
-    //added to queue
-    //doctor group a treats NEA queue
-    //doctor group b treats CW queue
-    //if NEA needs tests-> gets added to lab queue
- }
-
-
- public void run() {
- System.out.println("test");
-  
- }
 
 }

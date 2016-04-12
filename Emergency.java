d86df94b3dc18d86c11602b3b01f7041ce3eebbd
nelmarianna <aniela.opolski@gmail.com>
2016-04-11 23:01:56 -0400

    implemented thread

--- Emergency.java
+++ Emergency.java
@@ -1,17 +1,27 @@
 
-public class Emergency {
+public class Emergency implements Runnable{
 
 	
 	//has all the queues here???
-	public static void main(){
+	public static void main(String [] args){
 		
 		//person comes into emergency room
-		//nurse classifies CW or NEA
-		//added to queue
-		//doctor group a treats NEA queue
-		//doctor group b treats CW queue
-		//if NEA needs tests-> gets added to lab queue
+
 		
+		//three new threads one per nurse???
+		(new Thread(new Emergency())).start();
+		(new Thread(new Emergency())).start();
+		
+		//nurse classifies CW or NEA
+				//added to queue
+				//doctor group a treats NEA queue
+				//doctor group b treats CW queue
+				//if NEA needs tests-> gets added to lab queue
+	}
+
+
+	public void run() {
+	System.out.println("test");
 		
 	}
 }
